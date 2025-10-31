61a34700caefa9dc1058a21160dd7b0088979b0d
BillL856 <billl14@nycstudents.net>
2025-10-31 09:26:10 -0400

    git

---
+++ pet.py
@@ -0,0 +1,22 @@
+#Lesson 1
+class hero:
+    def __init__(self, name, __money, inventory):
+        self.name=name
+        self.money=__money
+        self.inventory=inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Ninja=hero("Ninja", 50, ["Ninja Stars"])
+Ninja.buy({"title":"Sword", "atk":34, "value":25})
+print(Ninja.__dict__)
+
+
+#Lesson 2
+class pet:
+    def __init__(self, name, __happiness, status):
+        self.name=name
+        self.happiness=__happiness
+        self.status=status
+        
