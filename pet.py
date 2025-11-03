8805118a68ccb8821b8263f1265d3ee3a49f30a0
BillL856 <billl14@nycstudents.net>
2025-11-03 09:24:00 -0500

    vfnjosqfe

--- pet.py
+++ pet.py
@@ -1,4 +1,4 @@
-#Lesson 1
+""" #Lesson 1
 class hero:
     def __init__(self, name, __money, inventory):
         self.name=name
@@ -11,16 +11,20 @@ class hero:
 Ninja=hero("Ninja", 50, ["Ninja Stars"])
 Ninja.buy({"title":"Sword", "atk":34, "value":25})
 print(Ninja.__dict__)
-
+ """
 
 #Lesson 2
 class pet:
-    def __init__(self, name, __happiness):
+    def __init__(self, name, happiness):
         self.name=name
-        self.happiness=__happiness
+        self.__happiness=happiness
         
     def play(self, act):
-        act=["Pet", "Fetch", "Play with Toy"]
-        EE=input("Play or No Play")
+        
+            self.__happiness+=10
 
+    def show_status(self):
+        show=input("Want to know the pet's happiness. Yes or No")
+        if show=="Yes":
+            print(f"{self.name}'s happiness is now {self.__happiness}")
         
