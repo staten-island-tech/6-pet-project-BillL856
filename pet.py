51a2d0080cdeba64351e5536108d8dfdd7346366
BillL856 <billl14@nycstudents.net>
2025-11-06 09:25:40 -0500

    BBGTBBGT

--- pet.py
+++ pet.py
@@ -14,42 +14,60 @@ print(Ninja.__dict__)
  """
 
 #Lesson 2
+why="true"
 class pet:
     def __init__(self, name, happiness):
         self.name=name
         self.__happiness=happiness
+    
         
     def play(self, act):
-        action= [
+        self.act=act
+        self.act= [
             {
-                "act": "fetch",
+                "act": "fetching",
                 "p":10
             },
             {
-                "act": "bellyrubs",
+                "act": "getting bellyrubs",
                 "p":15
             },
             {
-                "act": "walking",
+                "act": "going walking",
                 "p": 5
             },
             {
-                "act": "toys",
+                "act": "with toys",
                 "p":20
             }
         ]
-        print(action)
-        self.__happiness+=action
-        print(f"{self.name} is playing {act}")
+        
+        print(self.act)
+        do=input(f"To play with {self.name}, type Play, to find their happiness level, type Status, and to exit, type Close")
+        while do.lower()=="play" or do.lower()=="continue":
+            action=input(f"What do you want to do with {self.name}.")
+            for e in self.act:
+                if action.lower()==e["act"].lower():
+                    self.__happiness+=e["p"]
+                    print(f"{self.name} is {e}")
+            do=input("Continue, or close, or show status")
+        if do.lower()=="status":
+            self.status=="Yes"
+            do=input("Continue, or close")
+        elif do.lower()=="close":
+            why="false"
 
-    def show_status(self):
-        show=input("Want to know the pet's happiness. Yes or No")
-        if show=="Yes":
+
+    def show_status(self, status):
+        self.status=status
+        if status=="Yes":
             print(f"{self.name}'s happiness is now {self.__happiness}")
-
 Greg=pet("Greg", 60)
-do=input("To play with Greg, type Play, to find his happiness level, type Status, and to exit, type Close")
-if do=="play".lower():
+
+    
+
+    
+
     
 
 
