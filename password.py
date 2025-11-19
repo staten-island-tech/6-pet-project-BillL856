76e14414c2d21fb483c44c966c43e9314bf7a9b9
BillL856 <billl14@nycstudents.net>
2025-11-19 08:47:52 -0500

    Update password.py

--- password.py
+++ password.py
@@ -3,9 +3,12 @@ def passage(email, password):
     pvalidation=False
     if "@" not in email:
         return "INVALID. Retry Email"
+    elif "@" in email:
+        evalidation=True
     for i in password:
         if password(i) is i.upper():
             capital=True
         elif password(i) is i.digit():
             number=True
-    if len(password)>=8 and capital=
+    if len(password)>=8 and capital==True and number==True:
+        pvalidation=True
