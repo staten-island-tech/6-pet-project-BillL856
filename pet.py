6140e1aff37e3765621ed6172e91d1bc2d3c602d
BillL856 <billl14@nycstudents.net>
2025-11-25 09:07:46 -0500

    green man

--- pet.py
+++ pet.py
@@ -32,34 +32,16 @@ class pet:
             print(f"{self.name} is now {self.act}")
             if self.energy<30:
                 self.happiness-=15
-                doin=input("Continue or Sleep")class pet:
-    def __init__(self, name, __happiness, __energy, __hunger):
-        self.name=name
-        self.happiness=__happiness
-        self.energy=__energy
-        self.hunger=__hunger
-
-    def play(self, act):
-        self.act=act
-        do=input(f"To play with {self.name}, type Play")
-        doing=""
-        while do.lower() in ("play") or doing.lower() in ("continue"):
-            self.happiness+=7
-            self.energy-=6
-            self.hunger-=3
-            print(f"{self.name} is now {self.act}")
-            if self.energy<30:
-                self.happiness-=15
                 doin=input("Continue or Sleep")
-                if doin.lower()=="sleep" or doi.lower()=="sleep":
-                    self.slep
-                    doin=input("Continue or Eat")
+                if doin.lower()=="sleep":
+                    self.slep()
+                    doin=input("Continue")
             if self.hunger<20:
                 self.happiness-=20
                 doi=input("Continue or Eat")
-                if doi.lower()=="eat" or doin.lower()=="eat":
-                    self.food({"Dog Food"})
-                    doi=input("Continue or Sleep")
+                if doi.lower()=="eat":
+                    self.food("Dog Food")
+                    doi=input("Continue")
             self.show_status()
             self.dead()
             doing=input(f"Continue") 
@@ -87,7 +69,6 @@ class pet:
         if self.hunger <= 0 or self.energy <= 0 or self.happiness <= 0:
             print(f"{self.name} is dead.")
             raise SystemExit
-
 
 Greg=pet("Greg", 50, 50, 50)
 Greg.play("Playing Fetch")
